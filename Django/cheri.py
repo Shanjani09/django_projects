3a93855a52ce7e1e038eb3fd3e6c9f5a312fe163
Shanjani09 <shanjanisridurgadusanapudi@gmail.com>
2025-03-23 23:09:50 +0530

    23-09-2025

--- Django/cheri.py
+++ Django/cheri.py
@@ -1,36 +1,3 @@
-# import cherrypy
-# import os
-# class MyApp:
-#     @cherrypy.expose
-#     def index(self):
-#         return "This is a text...!"
-#     @cherrypy.expose
-#     def external_html(self):
-#         cherrypy.response.headers['Content-Type'] = 'text/html'
-#         try:
-#             file_path = os.path.join(os.path.dirname(__file__), 'externalfile.html')
-#             with open(file_path, 'r') as f:
-#                 return f.read()
-#         except FileNotFoundError:
-#             return "Error: external_page.html not found."
-#     @cherrypy.expose
-#     def html(self):
-#         cherrypy.response.headers['Content-Type'] = 'text/html'
-#         return """
-#         <html>
-#             <head>
-#                 <title>CherryPy HTML Page.</title>
-#             </head>
-#             <body>
-#                 <h1>Hello World! Welcome to CherryPy!</h1>
-#                 <p>This is an HTML page by Cherrypy</p>
-#             </body>
-#         </html>
-#         """
-# if __name__ == '__main__':
-#     cherrypy.quickstart(MyApp())
-
-
 import cherrypy
 import os
 class MyApp:
@@ -45,7 +12,7 @@ class MyApp:
             with open(file_path, 'r') as f:
                 return f.read()
         except FileNotFoundError:
-            return "Error: external_page.html not found."
+            return "Error: externalfile.html not found."
     @cherrypy.expose
     def html(self):
         cherrypy.response.headers['Content-Type'] = 'text/html'
